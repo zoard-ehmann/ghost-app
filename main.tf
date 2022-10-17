99b45cd984a99499fe6768740328bbc7583c6922
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-10-17 15:46:40 +0200

    feat: Add public subnets for Ghost VPC

--- main.tf
+++ main.tf
@@ -22,3 +22,36 @@ resource "aws_vpc" "ghost" {
     Project = "cloudx"
   }
 }
+
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.ghost.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name    = "public_a"
+    Project = "cloudx"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id            = aws_vpc.ghost.id
+  cidr_block        = "10.10.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name    = "public_b"
+    Project = "cloudx"
+  }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id            = aws_vpc.ghost.id
+  cidr_block        = "10.10.3.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name    = "public_c"
+    Project = "cloudx"
+  }
+}
