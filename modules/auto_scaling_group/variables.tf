a7391abf319b77613d8d31fba083244e08139b62
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-10-19 17:38:49 +0200

    fix: Remove unused variable from ASG module

--- modules/auto_scaling_group/variables.tf
+++ modules/auto_scaling_group/variables.tf
@@ -38,11 +38,6 @@ variable "vpc_zone_identifier" {
   type        = list(string)
 }
 
-variable "launch_template_id" {
-  description = "ID of the launch template"
-  type        = string
-}
-
 variable "lb_target_group_arn" {
   description = "ARN of the target group"
   type        = string
