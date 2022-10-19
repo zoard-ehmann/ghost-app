8e14658c976cdba5107c2a0cc264768962028ed2
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-10-19 15:26:37 +0200

    fix: Correct output references in load balancer

--- modules/load_balancer/outputs.tf
+++ modules/load_balancer/outputs.tf
@@ -1,9 +1,9 @@
 output "lb_dns_name" {
   description = "DNS name of the ALB"
-  value       = aws_lb.ghost.dns_name
+  value       = aws_lb.this.dns_name
 }
 
 output "lb_target_group_arn" {
   description = "ARN of the target group"
-  value       = aws_lb_target_group.ghost.arn
+  value       = aws_lb_target_group.this.arn
 }
